1d2615fa09478bbca789fc42fe33b7e47163191b
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-07-17 23:04:18 +0200

    Create post.js

---
+++ assets/js/post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var url = 'https://adriaan-telegram.herokuapp.com/notify/message';
+var request = new XMLHttpRequest();
+request.open('POST', url, true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send({
+  message: 'Test'
+);
