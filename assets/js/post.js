a5a9420f51b378613aa1a2b9fd82bfc646feeccb
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-07-17 23:23:56 +0200

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,9 +1,13 @@
 'use strict';
 
-var url = 'https://adriaan-telegram.herokuapp.com/notify/message';
-var request = new XMLHttpRequest();
-request.open('POST', url, true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({
-  message: 'Test'
-}));
+function post() {
+  var input = document.getElementById('email');
+  var url = 'https://adriaan-telegram.herokuapp.com/notify/message';
+  var request = new XMLHttpRequest();
+  request.open('POST', url, true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({
+    message: input.value
+  }));
+  input.value = '';
+}
