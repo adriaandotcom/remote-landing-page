e34f17923d64369513d2dfcbb10e7c67551509c3
Adriaan <adriaanvanrossum@users.noreply.github.com>
2017-07-17 23:21:10 +0200

    Use JSON stringify

--- assets/js/post.js
+++ assets/js/post.js
@@ -4,6 +4,6 @@ var url = 'https://adriaan-telegram.herokuapp.com/notify/message';
 var request = new XMLHttpRequest();
 request.open('POST', url, true);
 request.setRequestHeader('Content-Type', 'application/json');
-request.send({
+request.send(JSON.stringify({
   message: 'Test'
-});
+}));
